6baa794591b0bee641f76da0a5527da99d416381
heartfreeforever <1765578099@qq.com>
2020-03-11 15:30:59 +0800

    rm GA_01KP/testCSV.py

--- GA_01KP/testCSV.py
+++
@@ -1,8 +0,0 @@
-f = open("./data","r")   #设置文件对象
-line = f.readline()
-line = line[:-1]
-while line:             #直到读取完文件
-    print(float(line.split()[0]))
-    line = f.readline()  #读取一行文件，包括换行符
-    line = line[:-1]     #去掉换行符，也可以不去
-f.close() #关闭文件
